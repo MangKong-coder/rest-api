4283fdfd1ded330e3e1892e3771e93a11e7b3187
James Domingo <domango1214@gmail.com>
2021-12-13 00:24:15 +0800

    Converted promises to async await

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -20,33 +20,27 @@ const postValidation = (post) => {
     }
 }
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
-    let totalItems;
-    Post.find().countDocuments()
-    .then(count => {
-        totalItems = count;
-       return Post.find()
-           .skip((currentPage - 1) * perPage)
-           .limit(perPage)
-}).then(
-    posts => {
+    try {
+        const totalItems = await Post.find().countDocuments()
+        const posts = await Post.find()
+               .skip((currentPage - 1) * perPage)
+               .limit(perPage)
         res.status(200).json({
             posts: posts, 
             totalItems: totalItems
         })
-    }
-)    .catch(err => {
+
+    } catch (err) {
         errorFunc(err)
     next(err)
-
     }
-    )
-    
-};
+}
 
-exports.postPost = (req, res, next) => {
+
+exports.postPost = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         const error = new Error('Validation failed, entered data is invalid.');
@@ -62,7 +56,6 @@ exports.postPost = (req, res, next) => {
     const imageUrl = req.file.path.replace("\\","/");
     const title = req.body.title;
     const content = req.body.content;
-    let creator
     const post = new Post({
         title: title, 
         content: content, 
@@ -70,48 +63,40 @@ exports.postPost = (req, res, next) => {
         creator: req.userId   
     })
     post.save()
-    .then(result => {
-        return User.findById(req.userId)
-    })
-    .then(user => {
-        creator = user;
-        user.posts.push(post);
-        return user.save()
-    })
-    .then(result => {
+    try {
+        const creator = await User.findById(req.userId)
+        creator.posts.push(post);
+        creator.save()
         res.status(201).json({
             message: 'Post created successfully',
             post: post,
             creator: {
                 _id: creator._id, name: creator.name
             }
-        })
-        
-    })
-    .catch(err => {
-        errorFunc(err)
-    next(err)
+            })
+        } catch (err) {
+            errorFunc(err)
+            next(err)
+        }
 
-    });
     }
 
-    exports.getPost = (req, res, next) => {
+    exports.getPost = async (req, res, next) => {
         const postId = req.params.postId;
-        Post.findById(postId)
-        .then(post => {
+        try  {
+            const post = await Post.findById(postId)
             postValidation(post);
             res.status(200).json({
                 message: 'Post fetched.', 
                 post: post
             })
-        })
-        .catch(err => 
-            {
-                errorFunc(err)
-    })
+        } catch (err) {
+            errorFunc(err)
+            next(err)
+        }
     }
 
-    exports.updatePost = (req, res, next) =>{
+    exports.updatePost = async (req, res, next) =>{
         const postId = req.params.postId;
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -130,10 +115,10 @@ exports.postPost = (req, res, next) => {
             error.statusCode = 422;
             throw error
         }
-        Post.findById(postId)
-        .then(post => {
+        try {
+            const post = await Post.findById(postId)
             postValidation(post)
-            if (post.creator.toString !== req.userId) {
+            if (post.creator.toString() !== req.userId) {
                 const error = new Error ('Not authenticated')
                 error.statusCode = 403;
                 throw error
@@ -144,29 +129,23 @@ exports.postPost = (req, res, next) => {
             post.title = title;
             post.image = imageUrl;
             post.content = content;
-            return post.save()
-        })
-        .then(result => {
+            const result = await post.save()
             res.status(200).json({
                 message: 'Post Updated!',
                 post: result
             })
-        })
-        .catch(err => 
-            {
-                errorFunc(err)
-    next(err)
-
-    })
+        } catch (err) {
+            errorFunc(err)
+            next(err)
+        }
     };
 
     
 
-    exports.deletePost = (req, res, next) => {
+    exports.deletePost = async (req, res, next) => {
         const postId = req.params.postId;
-        Post.findById(postId)
-        .then(post =>{
-            // Check logged in user
+        try {
+            const post = await Post.findById(postId)
             postValidation(post)
             if (post.creator.toString() !== req.userId) {
                 const error = new Error ('Not authenticated')
@@ -174,75 +153,56 @@ exports.postPost = (req, res, next) => {
                 throw error
             }
             clearImage(post.imageUrl)
-            return Post.findByIdAndRemove(postId)
-        })
-        .then(result => {
-            return User.findById(req.userId)
-            
-        })
-        .then(user => {
+            const result = await Post.findByIdAndRemove(postId)
+            const user = await User.findById(req.userId)
             user.posts.pull(postId)
-            return user.save()
-        })
-        .then(result => {
+            user.save()
             res.status(200).json({
                 message: 'Deleted Post'
             })
-        })
-        .catch( err =>
-            {
-                errorFunc(err)
-    next(err)
-
-    })
+        } catch (err) {
+            errorFunc(err)
+            next(err)
+        }
     }
 
-    exports.getStatus = (req, res, next) => {
+    exports.getStatus = async (req, res, next) => {
         let status;
-        return User.findById(req.userId)
-        .then(user => {
+        try {
+            const user = await User.findById(req.userId)
             if (!user) {
                 const error = new Error('Not authenticated')
                 error.statusCode = 401;
                 throw error;
             }
             status = user.status;
-        })
-        .then(result => {
             res.status(200).json({status : status})
-        })
-        .catch(err =>
-            {
-                errorFunc(err)
+        } catch (err) {
+            errorFunc(err)
             next(err)
+        }
 
-    })
+}
 
-    }
-
-    exports.updateStatus = (req, res, next) => {
+    exports.updateStatus = async (req, res, next) => {
         const status = req.body.status
         let updatedStatus
-        User.findById(req.userId)
-        .then(user => {
+        try {
+            const user = await User.findById(req.userId)
             if (!user) {
                 const error = new Error('Not authenticated')
                 error.statusCode = 401;
                 throw error;
             }
             updatedStatus = user.status = status
-            return user.save();
-        })
-        .then(result => {
+            user.save();
             res.status(201).json({
                 message: 'Status updated!'})
-        })
-        .catch(err =>
-            {
-                errorFunc(err)
+            } catch (err) {
+            errorFunc(err)
             next(err)
+        }
 
-    })
     }
 
     const clearImage = filePath => {
